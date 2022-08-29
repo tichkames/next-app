45ba015f56063a1de5fbdb4c77e0de60080533af
Tich <tkausiyo@gmail.com>
2022-08-29 22:10:30 +0200

    configure typescript

--- pages/news/index.js
+++ pages/news/index.js
@@ -17,6 +17,13 @@ export default NewsArticlesList
 // You should use getServerSideProps when:
 // - Only if you need to pre-render a page whose data must be fetched at request time
 export const getServerSideProps = async (ctx) => {
+
+  const user = process.env.DB_USER
+  const password = process.env.DB_PASSWORD
+
+  console.log('user', user)
+  console.log('password', password)
+
   const response = await fetch('http://localhost:4000/news'); // your fetch function here
   const data = await response.json();
 
