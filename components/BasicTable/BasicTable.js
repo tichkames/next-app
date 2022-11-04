b155e5449dd2719d530f001a3dc9dba4085a56e2
Tich <tich@exokames.com>
2022-11-04 11:27:55 +0200

    Implemented react-table

---
+++ components/BasicTable/BasicTable.js
@@ -0,0 +1,48 @@
+import { useMemo } from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from '../../data/MOCK_DATA.json';
+import { COLUMNS } from './columns';
+import styles from '../../styles/BasicTable.module.css';
+
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable({
+    columns,
+    data,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column, i) => (
+              <th key={i} {...column.getHeaderProps}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr key={i} {...row.getRowProps()}>
+              {row.cells.map((cell, i) => (
+                <td key={i} {...cell.getCellProps()}>
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
---
+++ pages/table/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BasicTable } from '../../components/BasicTable/BasicTable';
+
+const Table = () => {
+  return <BasicTable />;
+};
+
+export default Table;
